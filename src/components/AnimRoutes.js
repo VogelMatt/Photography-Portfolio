4c046f9bfad303efa2fc2303c48171cf50740c2d
ToastyandTasty <mahvogel@gmail.com>
2024-01-21 23:58:55 -0600

    quick add

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -8,12 +8,14 @@ import Contact from '../pages/Contact'
 import { Routes, Route, useLocation } from 'react-router-dom'
 
 const AnimRoutes = () => {
-  return <Routes>
-    <Route Path '/' element={<Home />} />
-    <Route Path '/' element={<Home />} />
-    <Route Path '/' element={<Home />} />
-    <Route Path '/' element={<Home />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+  );
 };
 
 export default AnimRoutes;
