ed3fcd21c2f1ae55b8ac2f7c5f164645631bc31d
ToastyandTasty <mahvogel@gmail.com>
2024-01-24 23:57:10 -0600

    color

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,11 @@ import React from 'react';
 
 
 const Header = () => {
-  return <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>Header</header>;
+  return (
+    <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+      Header
+    </header>
+  );
 };
 
 export default Header;
