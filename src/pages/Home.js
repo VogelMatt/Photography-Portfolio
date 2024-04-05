3b80c4441a67692c6f3db4c2bfa9ae2ebeca413b
ToastyandTasty <mahvogel@gmail.com>
2024-04-04 23:57:14 -0500

    home page alteration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import WomanImg from '../img/home/woman.png';
+
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <section className='section bg-blue-200 '>
-    Home
+  return <section className='section'>
+    <div className="container mx-auto">
+      { /*text img wrapper */}
+      <div>
+        
+      </div>
+    </div>
   </section>;
 };
 
